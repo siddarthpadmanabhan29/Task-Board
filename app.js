faf1c7414ca50e47eadecb27d46ff3f50de1396f
Siddarth Padmanabhan <siddarthrp15@hotmail.com>
2025-10-21 20:15:01 -0400

    Initial Task Board setup

---
+++ app.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDYDdmjpTSOOMO4ProrLPAAC3akfiNHbn4",
+  authDomain: "task-board-4635f.firebaseapp.com",
+  projectId: "task-board-4635f",
+  storageBucket: "task-board-4635f.firebasestorage.app",
+  messagingSenderId: "1076154427387",
+  appId: "1:1076154427387:web:6d2a8e90fceae445d60d5e"
+};
